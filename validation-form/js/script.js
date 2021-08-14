3202f0b2a570a903193ebe50d09e9318229fabe2
DonHansCL <hansenglishteacher@gmail.com>
2021-08-13 20:29:46 -0400

    gsrfg

--- validation-form/js/script.js
+++ validation-form/js/script.js
@@ -1,1 +1,11 @@
-alert("HOla");
+console.log("Hola a todos, 3 + 4");
+console.log(3 + 4);
+console.log(5 - 1);
+console.log(4 * 2);
+console.log(9 / 3);
+
+console.log('hi' + 'ya')
+console.log('wo' + 'ah')
+console.log('Amo ' + 'Codear.')
+
+function mostrarmensaje
